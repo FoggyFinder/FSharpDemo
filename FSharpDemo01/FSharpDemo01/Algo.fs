1018d276ec1fa63d89b203bce9d6789d6656730a
robert.fuszenecker <robert.fuszenecker@dealogic.com>
2016-08-04 22:17:01 +0200

    Added performance test

---
+++ FSharpDemo01/FSharpDemo01/Algo.fs
@@ -0,0 +1,38 @@
+module Algo
+
+    let Polynomial = 0x90000001u;
+
+    let Summarize (state: uint32) (start: uint64) (n: int): uint64 =
+        let mutable Shreg:uint32 = 0u
+        let mutable accumulator = start
+        Shreg <- state;
+
+        let Next (): uint32 =
+            Shreg <- Shreg <<< 1;
+
+            if Shreg &&& 0x80000000u <> 0u then
+                Shreg <- Shreg ^^^ Polynomial
+
+            Shreg
+
+        for i = 1 to n do
+            accumulator <- accumulator + (uint64)(Next ())
+
+        accumulator
+
+    // Performance test runner
+    open System.Diagnostics
+
+    let startPerformanceTest () : unit =
+
+        let startState = 2u
+        let startSum = 0uL
+        let n = 2000000000
+
+        let sw = new Stopwatch ()
+
+        sw.Start ()
+        printfn "F#: "
+        let result = Summarize startState startSum n
+        printfn "Eredmény: %A" result
+        printfn "Idő: %A" sw.Elapsed
--- FSharpDemo01/FSharpDemo01/FSharpDemo01.fs
+++ FSharpDemo01/FSharpDemo01/FSharpDemo01.fs
@@ -1,6 +1,24 @@
 module FSharpDemo01
 
+// #r """..\..\packages\RestSharp\lib\net45\RestSharp.dll"""
+open RestSharp
+
+open Algo
+
+let getHup () =
+    let client = RestClient ("http://hup.hu/tracker")
+    
+    RestRequest (Method.GET)
+    |> client.Execute
+    |> fun x -> x.Content
+
+// [1..1000]
+// |> Seq.iter (function x -> getHup() |> ignore)
+
 [<EntryPoint>]
 let main argv =
+    printfn "Starting Performance test:"
+    startPerformanceTest ()
+
     printfn "%A" argv
     0 // return an integer exit code
