d7d69eedafe0c58cfb3641e45c666c23654c64a0
robert.fuszenecker <robert.fuszenecker@dealogic.com>
2016-08-31 20:52:29 +0200

    Added build system

--- build.fsx
+++ build.fsx
@@ -1,19 +1,24 @@
-#r "./packages/FAKE/tools/FakeLib.dll" // include Fake lib
-open Fake 
+#reference "./packages/FAKE/tools/FakeLib.dll"
+open Fake
 
-let fableCompiler = "node_modules/.bin/fable.cmd"
-let fableArgs = "-m commonjs -s"
-let fableFiles = "main.fsx"
+let sourceFiles = [
+    "main.fsx"
+]
 
 Target "Restore" (fun _ ->
     Paket.Restore (fun x -> x)
 )
 
-Target "Build" (fun _ ->
+Target "Build JS" (fun _ ->
+    let fableCompiler = "node_modules/.bin/fable.cmd"
+    let fableArgs = "-m commonjs -s"
+
+    let sources = sourceFiles |> String.concat ","
+
     let result = ExecProcessAndReturnMessages (fun info ->
         info.FileName <- fableCompiler 
         info.WorkingDirectory <- "." 
-        info.Arguments <- fableArgs + " " + fableFiles) (System.TimeSpan.FromMinutes 5.0)
+        info.Arguments <- fableArgs + " " + sources) (System.TimeSpan.FromMinutes 5.0)
 
     result.Messages
     |> Seq.iter (printfn "FABLE Output: %A") 
@@ -22,7 +27,18 @@ Target "Build" (fun _ ->
         failwithf "%A returned with a non-zero exit code" fableCompiler
 )
 
-"Restore"
-==> "Build"
+Target "Build EXE" (fun _ ->
+    sourceFiles
+    |> FscHelper.Compile [
+            FscHelper.Out "main.exe"
+            FscHelper.Target FscHelper.TargetType.Exe
+            // FscHelper.Debug false
+            FscHelper.Checked true
+        ]
+)
 
-Run "Build"
+"Restore"
+==> "Build JS"
+==> "Build EXE"
+
+Run "Build EXE"
