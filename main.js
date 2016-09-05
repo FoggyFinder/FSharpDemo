1641f439fb424c3d83c8e8305c45271f42a3d85a
robert.fuszenecker <robert.fuszenecker@dealogic.com>
2016-09-05 17:39:00 +0200

    Fixed typo in presentation

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.NativeInteropModule = exports.ComputationalExpressions = exports.AsyncProgramming = exports.UnitsOfMeasure = exports.Events = exports.Exceptions = exports.Interfaces = exports.Classes = exports.ControlFlow = exports.FunctionsAndCurrying = exports.Structs = exports.ActivePatterns = exports.PatternMatching = exports.OptionType = exports.Generics = exports.DiscriminatedUnions = exports.Enums = exports.TuplesAndRecords = exports.Collections = exports.TypeAliases = exports.Types = exports.LetsTranscend = exports.Introduction = undefined;
+exports.ComputationalExpressions = exports.AsyncProgramming = exports.UnitsOfMeasure = exports.Events = exports.Exceptions = exports.Interfaces = exports.Classes = exports.ControlFlow = exports.FunctionsAndCurrying = exports.Structs = exports.ActivePatterns = exports.PatternMatching = exports.OptionType = exports.Generics = exports.DiscriminatedUnions = exports.Enums = exports.TuplesAndRecords = exports.Collections = exports.TypeAliases = exports.Types = exports.LetsTranscend = exports.Introduction = undefined;
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -672,14 +672,5 @@ var ComputationalExpressions = exports.ComputationalExpressions = function ($exp
 
   var good_ = $exports["good'"] = divideByWorkflow_(12, 3, 2, 1);
   var bad_ = $exports["bad'"] = divideByWorkflow_(12, 3, 0, 1);
-  return $exports;
-}({});
-
-var NativeInteropModule = exports.NativeInteropModule = function ($exports) {
-  var Beep = $exports.Beep = function Beep(frequency, length) {
-    throw "extern was not given a DllImport attribute";
-  };
-
-  Beep(700, 1500);
   return $exports;
 }({});
