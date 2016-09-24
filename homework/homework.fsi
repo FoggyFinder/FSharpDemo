dbade43bed78bb0c1d71f59ff94908bce4f4a0a2
robert.fuszenecker <robert.fuszenecker@dealogic.com>
2016-09-24 13:31:09 +0200

    Homework rework

--- homework/homework.fsi
+++ homework/homework.fsi
@@ -1,3 +1,33 @@
+// --------------------------------------------------------------------
+// Baby level
+// --------------------------------------------------------------------
+
 val myMap : mapperFunction: ('T -> 'TResult) -> inputList: 'T list -> 'TResult list
-val myFilter : filterFunction: ('T -> bool) -> inputList:' T list -> 'T list
-val myReduce : reduceFunction: (int -> 'a -> int) -> inputList: 'a list -> int
+
+// [0..5]
+// |> myMap (fun x -> x * 3)
+// |> areEqual [0; 3; 6; 9; 12; 15]
+
+// --------------------------------------------------------------------
+// Intermediate level
+// --------------------------------------------------------------------
+
+val myFilter : filterFunction: ('T -> bool) -> inputList: 'T list -> 'T list
+
+// [0..20]
+// |> myFilter (fun x -> x % 3 = 0)
+// |> areEqual [0; 3; 6; 9; 12; 15; 18]
+
+// --------------------------------------------------------------------
+// Superhero level
+// --------------------------------------------------------------------
+
+val myReduce : reduceFunction: ('TAcc -> 'T -> 'TAcc') -> initialValue: 'TAcc -> inputList: 'T list -> 'TAcc
+
+// [0..5]
+// |> myReduce (fun a v -> a + v) 10
+// |> areEqual 25
+
+// [1;2;3;4;5]
+// |> myReduce (*) 1
+// |> areEqual 120
--- homework/homework.fsx
+++ homework/homework.fsx
@@ -1,7 +1,24 @@
+#r @"../packages/NUnit/lib/net45/nunit.Framework.dll"
+open NUnit.Framework
+
+let areEqual x y = Assert.AreEqual(x, y)
+
+// --------------------------------------------------------------------
+// Map
+// --------------------------------------------------------------------
+
 let rec myMap mapperFunction inputList =
     match inputList with
     | [] -> []
-    | x :: xs -> mapperFunction x :: myMap mapperFunction xs
+    | x :: xs -> (mapperFunction x) :: (myMap mapperFunction xs)
+
+[0..5]
+|> myMap (fun x -> x * 3)
+|> areEqual [0; 3; 6; 9; 12; 15]
+
+// --------------------------------------------------------------------
+// Filter
+// --------------------------------------------------------------------
 
 let rec myFilter filterFunction inputList =
     match inputList with
@@ -9,15 +26,28 @@ let rec myFilter filterFunction inputList =
     | x :: xs -> if filterFunction x then x :: myFilter filterFunction xs 
                     else myFilter filterFunction xs
 
-let rec myReduce reduceFunction inputList =
-    let rec myReduceInner reduceFunction a inputList =
+[0..20]
+|> myFilter (fun x -> x % 3 = 0)
+|> areEqual [0; 3; 6; 9; 12; 15; 18]
+
+// --------------------------------------------------------------------
+// Reduce
+// --------------------------------------------------------------------
+
+let rec myReduce reduceFunction initialValue inputList =
+    let rec myReduceInner reduceFunction accumulator inputList =
         match inputList with
-        | [] -> a
+        | [] -> accumulator
         | x :: xs ->
-            let a' = reduceFunction a x
-            myReduceInner reduceFunction a' xs
+            let accumulator' = reduceFunction accumulator x
+            myReduceInner reduceFunction accumulator' xs
 
-    myReduceInner reduceFunction 0 inputList
+    myReduceInner reduceFunction initialValue inputList
 
-// myReduce (fun a v -> a + v) [1;2;3;4;5]
-myReduce (+) [1;2;3;4;5]
+[0..5]
+|> myReduce (fun a v -> a + v) 10
+|> areEqual 25
+
+[1;2;3;4;5]
+|> myReduce (*) 1
+|> areEqual 120
