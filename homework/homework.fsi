131bf95b3c6186e41d1ca7006981148eda44ca73
robert.fuszenecker <robert.fuszenecker@dealogic.com>
2016-09-24 11:50:46 +0200

    Homework update

--- homework/homework.fsi
+++ homework/homework.fsi
@@ -1,3 +1,3 @@
-val myMap : mapperFunction:('T -> 'TResult) -> inputList:'T list -> 'TResult list
-val myFilter : filterFunction:('T -> bool) -> inputList:'T list -> 'T list
-
+val myMap : mapperFunction: ('T -> 'TResult) -> inputList: 'T list -> 'TResult list
+val myFilter : filterFunction: ('T -> bool) -> inputList:' T list -> 'T list
+val myReduce : reduceFunction: (int -> 'a -> int) -> inputList: 'a list -> int
--- homework/homework.fsx
+++ homework/homework.fsx
@@ -1,9 +1,23 @@
-let rec myMap f l =
-    match l with
+let rec myMap mapperFunction inputList =
+    match inputList with
     | [] -> []
-    | x :: xs -> f x :: myMap f xs
+    | x :: xs -> mapperFunction x :: myMap mapperFunction xs
 
-let rec myFilter f l =
-    match l with
+let rec myFilter filterFunction inputList =
+    match inputList with
     | [] -> []
-    | x :: xs -> if f x then x :: myFilter f xs else myFilter f xs
+    | x :: xs -> if filterFunction x then x :: myFilter filterFunction xs 
+                    else myFilter filterFunction xs
+
+let rec myReduce reduceFunction inputList =
+    let rec myReduceInner reduceFunction a inputList =
+        match inputList with
+        | [] -> a
+        | x :: xs ->
+            let a' = reduceFunction a x
+            myReduceInner reduceFunction a' xs
+
+    myReduceInner reduceFunction 0 inputList
+
+// myReduce (fun a v -> a + v) [1;2;3;4;5]
+myReduce (+) [1;2;3;4;5]
