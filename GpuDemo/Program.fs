4848cc4a7d68b345abffe35a58e05b8e52b4beae
robert.fuszenecker <robert.fuszenecker@dealogic.com>
2016-09-30 10:44:01 +0200

    Added better GPGPU demo

--- GpuDemo/Program.fs
+++ GpuDemo/Program.fs
@@ -6,27 +6,41 @@ open MathNet.Numerics
 open MathNet.Numerics.LinearAlgebra
 open System.Diagnostics
 
-[<EntryPoint>]
-let main argv = 
-    Control.NativeProviderPath <- Path.Combine(__SOURCE_DIRECTORY__,"../")
+module GpgpuModule =
 
-    printfn "Linear Algebra Provider = %A" Control.LinearAlgebraProvider
+    let compareSequences seq1 seq2 = 
+        // Compare the first 5 decimal digit of the double
+        Seq.compareWith (fun x y -> int ( x * 100000.0) - int (y * 100000.0)) seq1 seq2
+        |> (=) 0
 
-    let m = Matrix<double>.Build.Random(500, 500)
-    let v = Vector<double>.Build.Random(500)
+    [<EntryPoint>]
+    let main argv = 
+        let dataSize = 3000
 
-    let w = Stopwatch.StartNew()
-    let y1 = m.Solve(v);
-    printfn "Elapsed time: %A" w.Elapsed
-    printfn "y1 = %A" y1
+        let myMatrix = Matrix<double>.Build.Random(dataSize, dataSize)
+        let myVector = Vector<double>.Build.Random(dataSize)
+
+        Control.UseManaged();
+        printfn "Linear Algebra Provider = %A" Control.LinearAlgebraProvider
+
+        let stopwatch = Stopwatch.StartNew()
+        let solution1 = myMatrix.Solve(myVector);
+        let elapsed1 = stopwatch.Elapsed
+        printfn "Elapsed time: %A" elapsed1
+        printfn "y1 = %A" solution1
 
         // Using the Intel MKL native provider
-    Control.UseNativeMKL()
-    printfn "Linear Algebra Provider = %A" Control.LinearAlgebraProvider
+        //Control.NativeProviderPath <- Path.Combine(__SOURCE_DIRECTORY__, @"..\packages\MathNet.Numerics.MKL.Win.2.1.0\build\x64")
+        Control.UseNativeMKL()
+        printfn "Linear Algebra Provider = %A" Control.LinearAlgebraProvider
 
-    w.Restart()
-    let y2 = m.Solve(v);
-    printfn "Elapsed time: %A" w.Elapsed
-    printfn "y1 = %A" y2
+        stopwatch.Restart()
+        let solution2 = myMatrix.Solve(myVector);
+        let elapsed2 = stopwatch.Elapsed
+        printfn "Elapsed time: %A" stopwatch.Elapsed
+        printfn "y1 = %A" solution2
 
-    0 
+        printfn "Solution1 equals to Solution2 = %A" (compareSequences solution1 solution2)
+        printfn "Elapsed1 / Elapsed2 = %A" (elapsed1.TotalMilliseconds / elapsed2.TotalMilliseconds)
+
+        0 
