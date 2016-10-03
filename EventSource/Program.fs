cadec68439f5712e6181d3d28d94f9170a0221b9
robert.fuszenecker <robert.fuszenecker@dealogic.com>
2016-10-03 20:37:41 +0200

    Token update

--- EventSource/Program.fs
+++ EventSource/Program.fs
@@ -9,7 +9,7 @@ let myGithubCallback  notification =
 [<EntryPoint>]
 let main argv = 
     let client = new GitHubClient(new ProductHeaderValue("FSharpDemo"));
-    client.Credentials <- Credentials("853ca4d05a27db33747154c2119fd6921e08710b");
+    client.Credentials <- Credentials("d7633e43c0b5750fc5d94bdb9c8d5bc518b2f623");
 
     let observableClient = ObservableNotificationsClient(client)
 
