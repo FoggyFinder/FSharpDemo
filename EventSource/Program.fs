6e6a32b649b46f9f320540aead259bb645e71bcc
robert.fuszenecker <robert.fuszenecker@dealogic.com>
2016-10-03 20:33:06 +0200

    Added EventSource

---
+++ EventSource/Program.fs
@@ -0,0 +1,22 @@
+﻿open System
+open Octokit
+open Octokit.Reactive
+
+let myGithubCallback  notification = 
+    printfn "NOTI: %A" (notification.ToString())
+    ()
+
+[<EntryPoint>]
+let main argv = 
+    let client = new GitHubClient(new ProductHeaderValue("FSharpDemo"));
+    client.Credentials <- Credentials("853ca4d05a27db33747154c2119fd6921e08710b");
+
+    let observableClient = ObservableNotificationsClient(client)
+
+    use subscription = observableClient.GetAllForRepository("fuszenecker", "FSharpDemo").Subscribe(myGithubCallback)
+
+    printfn "Observing..."
+    Console.ReadLine() |> ignore
+
+    printfn "%A" argv
+    0 // return an integer exit code
