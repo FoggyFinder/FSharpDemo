a22e5342fd77b06d59e4f90825837ad431ee6c59
robert.fuszenecker <robert.fuszenecker@dealogic.com>
2016-10-04 20:59:20 +0200

    Added unit test project

---
+++ UnitTestDemo/UnitTests.fs
@@ -0,0 +1,21 @@
+﻿namespace UnitTestDemo
+
+open NUnit.Framework
+
+type SmallRecord = {
+    Name: string
+    Value: int option
+}
+
+type UnitTestClass() = 
+    let areEqual x y = Assert.AreEqual(x, y)
+
+    [<Test>]
+    member this.``This is the first test`` () =
+        [1..5]
+        |> areEqual [1; 2; 3; 4; 5]
+
+    [<Test>]
+    member this.``Structural equality test`` () =
+        { Name = "My little record"; Value = Some 200 }
+        |> areEqual { Name = "My little record"; Value = Some 200 }
